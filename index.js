a174f0b144a3ee982af66b4741396c8ec9348d5e
Nahid30 <nahidshuvo30@gmail.com>
2022-05-08 03:49:25 +0600

    load a single data for one user/id and this api send to the client side to make single cart. and used app.post to create data from clientsite

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
 
@@ -25,12 +25,28 @@ async function run() {
         await client.connect();
         const itemsCollection = client.db('freshFruits').collection('items');
 
-        app.get('/items', async (req, res) => {
+        app.get('/item', async (req, res) => {
             const query = {};
             const cursor = itemsCollection.find(query);
             const items = await cursor.toArray();
             res.send(items);
         });
+
+        app.get('/item/:id', async (req, res) =>{
+            const id = req.params.id;
+            const query = {_id:ObjectId(id)};
+            const items = await itemsCollection.findOne(query);
+            res.send(items);
+        })
+
+
+        //Post
+
+        app.post('/item', async(req, res) =>{
+            const newItem = req.body;
+            const result = await itemsCollection.insertOne(newItem);
+            res.send(result);
+        })
 
 
     }
